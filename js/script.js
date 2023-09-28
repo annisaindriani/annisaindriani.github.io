fd1838ee00f903673d0b3b64d7d8c134c70b2e9e
annisaindriani <annisa.indriani@binus.ac.id>
2023-09-28 21:35:47 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,52 @@
+/* function update greetings text */
+
+function updateGreeting(){
+    const iconElement = document.getElementById('greetings-icon');
+    const messageElement = document.getElementById('greetings-msg');
+    const now = new Date();
+    const hour = now.getHours();
+
+    let iconClass;
+    let message;
+
+    if(hour >= 5 && hour < 12){
+        iconClass = 'fa-cloud-sun'; 
+        message = 'Good Morning!';
+    } else if(hour >= 12 && hour < 18){
+        iconClass = 'fa-sun'; 
+        message = 'Good Afternoon!';
+    } else if(hour >= 18 && hour < 22){
+        iconClass = 'fa-cloud-moon'; 
+        message = 'Good Evening!';
+    } else{
+        iconClass = 'fa-moon'; 
+        message = 'Good Night!';
+    }
+
+    iconElement.classList.remove('fa-cloud-sun', 'fa-sun', 'fa-cloud-moon', 'fa-moon');
+    iconElement.classList.add(iconClass);
+    messageElement.textContent = message;
+}
+
+updateGreeting();
+setInterval(updateGreeting, 60000);
+
+/* function to show hidden text when the toggle is clicked */
+
+const toggleButtons = document.querySelectorAll('.toggle-button');
+const hiddenTexts = document.querySelectorAll('.hidden-text');
+
+toggleButtons.forEach((button, index) => {
+    button.addEventListener('click', () => toggleHiddenText(index));
+});
+
+function toggleHiddenText(index) {
+    const hiddenText = hiddenTexts[index];
+    // Toggle the display property of the corresponding hidden text
+    if (hiddenText.style.display === 'none' || hiddenText.style.display === '') {
+        hiddenText.style.display = 'block';
+    } else {
+        hiddenText.style.display = 'none';
+    }
+}
+
